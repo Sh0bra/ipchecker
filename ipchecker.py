98909ae946adcafbca072cace4e52eb43d075d4d
sh0bra <jshin1@cpp.edu>
2024-09-07 23:04:15 -0700

    create virustotal urls from ips

--- ipchecker.py
+++ ipchecker.py
@@ -1,8 +1,22 @@
+import sys
+import requests
+
+headers = {
+    "accept" : "application/json",
+    "x-apikey" : "<APIKEY>"
+}
 
 
-IPLIST = ['1','2']
+IPLIST = ['10.10.10.10','22.2.2.2']
+text = input("Enter IP address: ")
+print(text)
 
-def printIP():
-    print(IPLIST)
+def make_urls():
+    URLS = []
+    for ip in IPLIST:
+        url = "https://www.virustotal.com/api/v3/ip_addresses/" + ip
+        URLS.append(url)
+    return URLS    
 
-printIP()
+URLLIST = make_urls()
+print(URLLIST)
